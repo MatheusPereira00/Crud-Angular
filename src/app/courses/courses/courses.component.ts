ba1824db95b2f028c1200b67002a261ece8a69a1
MatheusPereira00 <matheuspereira@ibm.com>
2023-03-06 15:22:55 -0300

    Criação do backend

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+
+  courses: Course[]= [
+    {_id: '1', name: 'Angular', category: 'front-end'},
+    {_id: '2', name: 'JavaScript', category:'front-end'},
+    {_id: '3', name: 'Python', category: 'Backend'},
+    {_id: '4', name: 'Css', category: 'front-end'},
+    {_id: '5', name: 'Html', category: 'front-end'},
+    {_id: '6', name: 'React', category: 'front-end'}
+  ];
+  
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    //this.courses = [];
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
